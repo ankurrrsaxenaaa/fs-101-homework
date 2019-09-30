a97bd92e2c630913876d0dc1e2beae7976dca312
ankurrrsaxenaaa <ankurrrsaxenaaa@gmail.com>
2019-09-30 19:59:25 +0530

    Day 11 question 2 and 4 done

---
+++ daily/11-day/src/main/java/question03/ThreeSum.java
@@ -0,0 +1,43 @@
+package question03;
+
+import java.util.*;
+
+public class ThreeSum {
+    private static List<List<Integer>> threeSum(int[] nums)
+    {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        TreeSet<String> tempSet = new TreeSet<>();
+        List<Integer> triplet = new ArrayList<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                if (nums[i] + nums[j] + nums[k] == 0) {
+                    String str = nums[i] + ":" + nums[j] + ":" + nums[k];
+                    if (!tempSet.contains(str)) {
+                        triplet.add(nums[i]);
+                        triplet.add(nums[j]);
+                        triplet.add(nums[k]);
+                        result.add(triplet);
+                        triplet = new ArrayList<>();
+                        tempSet.add(str);
+                    }
+                    j++;
+                    k--;
+                } else if (nums[i] + nums[j] + nums[k] < 0)
+                    j++;
+                else
+                    k--;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args)
+    {
+        int[] nums = { -1, 0, 1, 2, -1, -4};
+        List<List<Integer>> result = threeSum(nums);
+            System.out.println(result);
+    }
+}
---
+++ daily/11-day/src/main/java/question04/JumpGame.java
@@ -0,0 +1,32 @@
+package question04;
+
+public class JumpGame {
+    private static boolean canJump(int nums[])
+    {
+        if (nums.length <= 1)
+            return true;
+        if (nums[0] == 0)
+            return false;
+        int maxReach = nums[0];
+        int step = nums[0];
+        for (int i = 1; i < nums.length; i++)
+        {
+            if (i == nums.length - 1)
+                return true;
+            maxReach = Math.max(maxReach, i+nums[i]);
+            step--;
+            if (step == 0)
+            {
+                if(i>=maxReach)
+                    return false;
+                step = maxReach - i;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args)
+    {
+        int nums[] = new int[] {2,3,1,1,4};
+        System.out.println(canJump(nums));
+    }
+}
