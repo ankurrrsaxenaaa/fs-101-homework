4af040d9ac895d2448344e1d69e5da0963d5d164
ankurrrsaxenaaa <ankurrrsaxenaaa@gmail.com>
2019-10-08 12:47:02 +0530

    Day 11 updated

---
+++ daily/11-day/src/main/java/chunker/Chunker.java
@@ -0,0 +1,35 @@
+package chunker;
+
+import java.lang.reflect.Array;
+public class Chunker<T> {
+    private final  Class<? extends T> cls;
+    public Chunker(Class<? extends T> cls) {
+        this.cls = cls;
+    }
+    public   <T> T[][] genericChunker(T[] input, int chunkSize)
+    {
+        int resultSize=input.length/chunkSize;
+        if(input.length%chunkSize!=0)
+        {
+            resultSize++;
+        }
+        T[][]  result = (T[][])Array.newInstance(cls, resultSize,chunkSize);//new T[resultSize][];
+        int counter=0;
+        for(int i=0;i<resultSize;i++)
+        {
+            T chunk[] = (T[])Array.newInstance(cls, chunkSize);
+            //  int chunk[]=new int[chunkSize];
+            if(i==resultSize-1)
+            {
+                chunk= (T[])Array.newInstance(cls, (input.length)-counter);
+                //new int[(input.length)-counter];
+            }
+            for(int j=0;j<chunk.length;j++)
+            {
+                chunk[j]=  input[counter++];
+            }
+            result[i]=chunk;
+        }
+        return result;
+    }
+}
