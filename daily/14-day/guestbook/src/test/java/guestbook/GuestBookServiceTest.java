7ba2cb8638d6143ecd2a34e8196c5467f0f26179
ankurrrsaxenaaa <ankurrrsaxenaaa@gmail.com>
2019-10-13 12:23:30 +0530

    Day 14 Homework Updated

--- daily/14-day/guestbook/src/test/java/guestbook/GuestBookServiceTest.java
+++ daily/14-day/guestbook/src/test/java/guestbook/GuestBookServiceTest.java
@@ -8,14 +8,14 @@ import static org.assertj.core.api.Assertions.*;
 public class GuestBookServiceTest {
 
     @Test
-    public void should_able_to_save_when_ip_address_is_new() {
+    public void should__be_able_to_save_when_ip_address_is_new() {
         GuestBookEntry guestBookEntry = new GuestBookEntry( "Shekhar", "Shekhar is busy today" );
         GuestBookService guestBookService = new GuestBookService( new CacheUtility() );
         guestBookService.createAndSaveEntry( guestBookEntry, "121" );
     }
 
     @Test
-    public void should_able_to_throw_exception_when_ip_address_is_repeated() {
+    public void should_be_able_to_throw_exception_when_ip_address_is_redundant() {
         GuestBookEntry guestBookEntry = new GuestBookEntry( "Shekhar", "Shekhar is busy today" );
         GuestBookService guestBookService = new GuestBookService( new CacheUtility() );
         guestBookService.createAndSaveEntry( guestBookEntry, "121" );
@@ -27,7 +27,7 @@ public class GuestBookServiceTest {
     }
 
     @Test
-    public void should_able_to_throw_exception_when_there_is_spam_in_content() {
+    public void should_be_able_to_throw_exception_when_there_is_spam_in_content() {
         GuestBookEntry guestBookEntry = new GuestBookEntry( "Ankur", "This is a spam message" );
         GuestBookService guestBookService = new GuestBookService( new CacheUtility() );
         try {
@@ -38,7 +38,7 @@ public class GuestBookServiceTest {
     }
 
     @Test
-    public void should_able_to_share_the_post_if_ip_is_new_and_content_so_not_contain_spam() {
+    public void should__be_able_to_share_the_post_if_ip_is_new_and_content_does_not_contain_spam() {
         GuestBookEntry guestBookEntry = new GuestBookEntry( "Ankur", "I am a finalist in XGT" );
         GuestBookService guestBookService = new GuestBookService( new CacheUtility() );
         guestBookService.createAndSaveEntry( guestBookEntry, "123" );
